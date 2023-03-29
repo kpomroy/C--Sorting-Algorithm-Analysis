192d938b789e3937c3b8faad92097aa4a405f4e2
kpomroy <kpomroy@uvm.edu>
2023-03-29 13:05:48 -0400

    Used 5 different sorting algorithms

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include "bubbleSort.h"
 #include "heapSort.h"
+#include "selectionSort.h"
+#include "mergeSort.h"
+#include "insertionSort.h"
+#include "twoSort.h"
 #include "Song.h"
+#include <fstream>
+#include <vector>
+
 
 using namespace std;
 
 int main() {
+    vector<Song> songs;
+
+    // Get data from csv of top 1000 streamed songs on Spotify
+    getDataFromFile("../spotify_songs.csv", songs);
+
+    //Bubble Sort
+    bubbleSort(songs);
+
+    //Selection Sort
+    selectionSort(songs);
+
+    //Merge Sort
+    mergeSort(songs);
+
+    //Heap Sort
+    heapSort(songs);
+
+    //Two Sort
+    twoSort(songs);
+
     return 0;
 }
